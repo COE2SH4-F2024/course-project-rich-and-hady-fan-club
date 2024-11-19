a7050524bbffbf9ee1d15ac30a3479eaf4ff4705
suthas4 <suthas4@mcmaster.ca>
2024-11-19 13:06:12 -0500

    athar tutorial week 10 tips commit

--- Player.cpp
+++ Player.cpp
@@ -7,22 +7,55 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos.pos->x = mainGameMechsRef->getBoardSizeX()/2;
+    playerPos.pos->y = mainGameMechsRef->getBoardSizeY()/2;
+    playerPos.symbol = '@';
 }
 
 
 Player::~Player()
 {
     // delete any heap members here
+    // no keyword new in the constructor - LEAVE EMPTY FOR NOW
 }
 
 objPos Player::getPlayerPos() const
 {
     // return the reference to the playerPos arrray list
+    return playerPos;
 }
 
 void Player::updatePlayerDir()
 {
-        // PPA3 input processing logic          
+    char input = mainGameMechsRef->getInput();
+    // PPA3 input processing logic  
+    switch(input)
+    {
+        case 'W':
+        case 'w':
+            if (myDir != UP && myDir != DOWN){
+                myDir = UP;
+            }
+            break;
+        case 'S':
+        case 's':
+            if (myDir != UP && myDir != DOWN){
+                myDir = UP;
+            }
+            break;
+        case 'A':
+        case 'a':
+            if (myDir != LEFT && myDir != RIGHT){
+                myDir = LEFT;
+            }
+            break;
+        case 'D':
+        case 'd':
+            if (myDir != LEFT && myDir != RIGHT){
+                myDir = RIGHT;
+            }
+            break;
+    }
 }
 
 void Player::movePlayer()
--- Project.cpp
+++ Project.cpp
@@ -2,11 +2,14 @@
 #include "MacUILib.h"
 #include "objPos.h"
 
+#include "Player.h"
+
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+Player *myPlayer; //Global pointer meant to instantiate a player object on the heap
+GameMechs *myGM;
 
 void Initialize(void);
 void GetInput(void);
@@ -22,7 +25,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(myGM->getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -40,7 +43,9 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    myGM = new GameMechs();
+    myPlayer = new Player(myGM);
+
 }
 
 void GetInput(void)
@@ -56,7 +61,11 @@ void RunLogic(void)
 void DrawScreen(void)
 {
 
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();  
+
+    objPos playerPos = myPlayer->getPlayerPos();
+
+    MacUILib_printf("Player [x, y, sym] = [%d, %d, %c]\n", playerPos.pos->x, playerPos.pos->y, playerPos.symbol);  
 }
 
 void LoopDelay(void)
@@ -68,6 +77,9 @@ void LoopDelay(void)
 void CleanUp(void)
 {
     MacUILib_clearScreen();    
+
+    delete myPlayer;
+    delete myGM;
 
     MacUILib_uninit();
 }
