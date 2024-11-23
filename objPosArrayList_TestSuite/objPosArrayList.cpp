b19cd2b2a51b1efcd5004164e699b6371f17f7dc
suthas4 <suthas4@mcmaster.ca>
2024-11-23 14:53:44 -0500

    fixed objPosArrayList

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -1,32 +1,30 @@
 #include "objPosArrayList.h"
 
+
 // Check lecture contents on general purpose array list construction, 
 // and modify it to support objPos array list construction.
 
 objPosArrayList::objPosArrayList()
 {
-    listSize = 0; // DOUBLE CHECK NUMBER
+
+    listSize = 0;
     arrayCapacity = ARRAY_MAX_CAP;
-
     aList = new objPos[arrayCapacity];
-    aList->pos->x = 0;
-    aList->pos->x = 0;
-    aList->symbol = '*';
-}
-    
 
+    for (int i = 0; i < arrayCapacity; i++) {
+        aList[i].pos->x = 0;
+        aList[i].pos->y = 0;
+        aList[i].symbol = '*';
+    }
+
+}
+
+// Destructor
 objPosArrayList::~objPosArrayList()
 {
-    // Delete objPos but idk how yet
+    // might need to delete all list members 
     delete[] aList;
 }
-
-// Copy Constructor
-
-
-// Copy Assignment Operator
-
-
 
 int objPosArrayList::getSize() const
 {
@@ -35,51 +33,47 @@ int objPosArrayList::getSize() const
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-    // Exits if the arrayCapacity is hit
-    if(listSize == arrayCapacity)
-    {
+
+
+    if(listSize == arrayCapacity){
         return;
     }
 
-    listSize++;     // Increase the size of the List by one
-    
-    for(int i = listSize; i>0; i--)
-    {
-        aList[i] = aList[i-1]; // Shifting all elements towards the tail to create space at the head
+    for(int i = listSize; i > 0; i--){
+        aList[i] = aList[i-1];
     }
-    // aList[0] is ready to be OVERWRITTEN by incoming value
+    aList[0] = thisPos;
 
-    aList[0] = thisPos; // Inserting the element at the head, index: 0
-    
+    listSize++;
+
+
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
+
     aList[listSize++] = thisPos;
+
 }
 
 void objPosArrayList::removeHead()
 {
-    if(listSize == 0)
-    {
+    if(listSize ==0){
         return;
     }
 
-    for(int i=0; i<listSize-1; i++)
-    {
-        aList[i] = aList[i+1];  // Shifting towards the head, element i replaced by element i+1
+    for(int i = 0; i < listSize-1; i++){
+        aList[i] = aList[i+1];
     }
-    listSize--; // Decrease the size of the list by one
+    listSize--;
 }
 
 void objPosArrayList::removeTail()
 {
-    if(listSize == 0)
-    {
+    if(listSize ==0){
         return;
     }
-
-    listSize--; // Decrease the size of the list by one, Lazy Delete
+    listSize--;
 }
 
 objPos objPosArrayList::getHeadElement() const
@@ -94,8 +88,7 @@ objPos objPosArrayList::getTailElement() const
 
 objPos objPosArrayList::getElement(int index) const
 {
-    if(index>=0 || index <= listSize)
-    {
-        return aList[index];
-    }
+
+    return aList[index];
+
 }
